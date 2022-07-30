fbb1ee6675436c0339193114dfa73dc3db53f7e7
Анатолий <anatoliy_25@ukr.net>
2022-07-30 20:49:49 +0300

    30.07.2022

---
+++ 26022022.py
@@ -0,0 +1,27 @@
+# sun of two numbers
+result = 1+1
+# print("Result", result)
+# print("Result", type("Result"))
+# print(result, type(result))
+
+
+# number_a = 7          # straight method
+# number_b = number_a   # reference to another object
+#
+# print(number_a, number_b)
+#
+# number_a, number_b = 4, 12 + 3  # multiple assignment exist, not recommended
+# print(number_a, number_b)
+#
+# value = 10
+
+# value int;
+# value = 3
+#
+# value str = "string"
+
+
+value_str = "string"
+value_int = 13
+result = value_str + value_int
+print(result)
