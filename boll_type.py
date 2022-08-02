22f615f706f919e5d7e339eceef613f59e9fe29a
Анатолий <anatoliy_25@ukr.net>
2022-08-02 19:08:35 +0300

    three tasks

--- boll_type.py
+++ boll_type.py
@@ -1,21 +1,40 @@
 ##### 1 #####
 
-comparison_result_1 = 3 < 5
-print(comparison_result_1)
-
-comparison_result_2 = 3 <= 5
-print(comparison_result_2)
-
-comparison_result_3 = 3 != 5
-print(comparison_result_3)
+# comparison_result_1 = 3 < 5
+# print(comparison_result_1)
+#
+# comparison_result_2 = 3 <= 5
+# print(comparison_result_2)
+#
+# comparison_result_3 = 3 != 5
+# print(comparison_result_3)
 
 ##### 2 #####
 
-right_result_1 = 5 == 5
-print(right_result_1)
+# right_result_1 = 5 == 5
+# print(right_result_1)
+#
+# right_result_2 = 5 >= 5
+# print(right_result_2)
+#
+# right_result_3 = 5 <= 5
+# print(right_result_3)
 
-right_result_2 = 5 >= 5
-print(right_result_2)
+##### 3 #####
 
-right_result_3 = 5 <= 5
-print(right_result_3)
+combination_1 = True and (True or False)
+print(combination_1)
+
+combination_2 = True or (True and False)
+print(combination_2)
+
+combination_3 = True or (True or False)
+print(combination_3)
+
+combination_4 = True and (True and not False)
+print(combination_4)
+
+combination_5 = (not True or True) or False
+print(combination_5)
+
+##### 4 #####
