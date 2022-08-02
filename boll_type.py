58eddc6d1ecb68da1bb3b7b7dbb2a58acd6c2de7
Анатолий <anatoliy_25@ukr.net>
2022-08-02 18:57:16 +0300

    two tasks

---
+++ boll_type.py
@@ -0,0 +1,21 @@
+##### 1 #####
+
+comparison_result_1 = 3 < 5
+print(comparison_result_1)
+
+comparison_result_2 = 3 <= 5
+print(comparison_result_2)
+
+comparison_result_3 = 3 != 5
+print(comparison_result_3)
+
+##### 2 #####
+
+right_result_1 = 5 == 5
+print(right_result_1)
+
+right_result_2 = 5 >= 5
+print(right_result_2)
+
+right_result_3 = 5 <= 5
+print(right_result_3)
